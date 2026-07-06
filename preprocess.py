d7b39cd642824d70dc5fca2111f1809bae408e62
Aditi <aditi2021choudhury@gmail.com>
2026-07-07 01:21:14 +0530

    Implement resume text preprocessing

---
+++ preprocess.py
@@ -0,0 +1,39 @@
+import re
+import nltk
+
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+nltk.download("punkt")
+nltk.download("stopwords")
+nltk.download("wordnet")
+
+lemmatizer = WordNetLemmatizer()
+
+stop_words = set(stopwords.words("english"))
+
+def preprocess_text(text):
+
+    # Convert text to lowercase
+    text = text.lower()
+
+    # Remove numbers
+    text = re.sub(r'\d+', '', text)
+
+    # Remove punctuation and special characters
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+
+    # Tokenize the text
+    words = word_tokenize(text)
+
+    # Remove stop words and lemmatize
+    cleaned_words = []
+
+    for word in words:
+        if word not in stop_words:
+            cleaned_word = lemmatizer.lemmatize(word)
+            cleaned_words.append(cleaned_word)
+
+    # Join words back into a sentence
+    return " ".join(cleaned_words)
